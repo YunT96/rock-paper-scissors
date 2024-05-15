54ccc728ef8b1271d1dec36258a4f87081a51042
YunT96 <163807078+YunT96@users.noreply.github.com>
2024-05-15 16:18:15 +0800

    Add game logic to check for winner
    Initialise score variables to keep tab of scores and create function to check for winner.

--- rps.js
+++ rps.js
@@ -1,8 +1,6 @@
 console.log("Script Loaded");
 
-
 //1. Generate computer choice using math.floor and math.random
-
 function getComputerChoice(){
     let comChoice = Math.floor(Math.random() * 3) + 1;
     
@@ -17,15 +15,11 @@ function getComputerChoice(){
             comChoice = "scissors";
             break;
     }
-    
+    console.log(comChoice);
     return comChoice;
 }
 
-console.log(`The computer has chosen: ${getComputerChoice()}`);
-
-
 //2. Use prompt to get user choice
-
 function getHumanChoice(){
     let humanChoice = prompt("Enter your choice: rock, paper or scissors");
     humanChoice = humanChoice.toLowerCase();
@@ -36,12 +30,36 @@ function getHumanChoice(){
     }
 
     return(humanChoice);
-    
 }
 
-console.log(`User has chosen: ${getHumanChoice()}`);
+//3. Declare a winner and use variables to keep track of scores
+let computerScore = 0;
+let humanScore = 0;
 
-//3. Use variables to keep track of scores
+function gameRound(getComputerChoice, getHumanChoice){
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+
+    if( computerChoice == humanChoice){
+        alert("Game was a tie");
+        gameRound(getComputerChoice, getHumanChoice);
+    }
+    else if(computerChoice == "rock" && humanChoice == "scissors"){
+        computerScore++;
+    }
+    else if(computerChoice == "paper" && humanChoice == "rock"){
+        computerScore++;
+    }
+    else if(computerChoice == "scissors" && humanChoice == "paper"){
+        computerScore++;
+    }
+    else{
+        humanScore++
+    }
+    
+}
+gameRound(getComputerChoice, getHumanChoice);
+console.log(`Computer score: ${computerScore} \nHuman score: ${humanScore}`);
 
 //4. Write a logice that allows the game to be played in rounds. 
 //   Increment score for winner of round
